b09c65713b43e7c771ffd4c12c6a1dfab4d8f890
sajadevo <nawabisajadahmad@gmail.com>
2024-12-13 16:55:38 +0430

    update projects carousel

--- app/components/projects-carousel.tsx
+++ app/components/projects-carousel.tsx
@@ -6,7 +6,7 @@ import { ProjectFrame } from "@/components";
 export function ProjectsCarousel() {
   return (
     <section className="relative">
-      <div className="absolute inset-0 size-full max-w-7xl mx-auto bg-white side-lines" />
+      <div className="absolute inset-0 size-full max-w-7xl mx-auto bg-white before:absolute before:z-0 before:h-full before:left-0 before:bg-secondary before:w-px after:absolute after:z-0 after:h-full after:right-0 after:bg-secondary after:w-px" />
       <div className="flex gap-12 relative overflow-x-auto snap-x snap-mandatory py-2 no-scrollbar">
         <ProjectFrame
           img="/product.png"
