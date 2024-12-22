4cee7c2df4b8abec3bf0f4807d569513c5ad4aaf
sajadevo <nawabisajadahmad@gmail.com>
2024-12-22 23:01:33 +0430

    update images

--- app/(app)/components/projects-carousel.tsx
+++ app/(app)/components/projects-carousel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // @components
 import { ProjectFrame } from "@/components";
 
@@ -6,7 +8,7 @@ import { getProjects } from "@/lib/payload";
 
 export async function ProjectsCarousel() {
   const { docs: projects } = await getProjects();
-
+  console.log(projects);
   return (
     <section>
       <div className="-mx-55 lg:-mx-70 xl:-mx-80 xxl:-mx-96 flex gap-6 border-x border-secondary lg:gap-12 will-change-scroll relative overflow-x-auto snap-x snap-mandatory py-2 no-scrollbar">
@@ -15,7 +17,7 @@ export async function ProjectsCarousel() {
           ({ id, title, description, image, status, path }: any) => (
             <ProjectFrame
               key={id}
-              img={image?.url}
+              img={`/media/${image?.filename}`}
               path={path}
               title={title}
               status={status}
