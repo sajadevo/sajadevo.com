fcd613b54e13eb0c83c9d8d5f23f9b5800dd648a
sajadevo <nawabisajadahmad@gmail.com>
2024-12-23 08:49:01 +0430

    update project frame

--- components/project-frame.tsx
+++ components/project-frame.tsx
@@ -28,7 +28,7 @@ export function ProjectFrame({
       href={isCompleted ? path : "#"}
       target={isCompleted ? "_blank" : "_self"}
       className={cn(
-        "group/project relative shrink-0 w-max h-60 sm:h-80 md:h-104 lg:h-116 xl:h-128 mx-auto bg-white border border-secondary p-4 sm:p-6 hover:border-primary transition-all duration-300 ease-in",
+        "group/project relative shrink-0 aspect-auto w-max h-60 sm:h-80 md:h-104 lg:h-116 xl:h-128 bg-white border border-secondary p-4 sm:p-6 hover:border-primary transition-all duration-300 ease-in",
         className
       )}
     >
