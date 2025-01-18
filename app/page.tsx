5a02bbac4b180bc909fee5ef572e02893465d11e
sajadevo <nawabisajadahmad@gmail.com>
2025-01-18 18:37:31 +0500

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,74 @@
 // @components
-import { Line } from "@/components";
-import {
-  Hero,
-  ProjectsCarousel,
-  Story,
-  Services,
-  Experience,
-  Contact,
-} from "./components";
+import Link from "next/link";
+import { Button } from "@/components";
+
+// @icons
+import { Contra } from "@/icons";
+
+const services = [
+  {
+    title: "Web Design",
+    description:
+      "Craft unique designs with style and precision. Build user-friendly solutions.",
+  },
+  {
+    title: "Web Development",
+    description:
+      "Develop robust, scalable, and efficient web solutions tailored to your needs.",
+  },
+  {
+    title: "Product Design",
+    description:
+      "Create innovative, user-centric designs that drive engagement and success.",
+  },
+];
 
 export default async function Home() {
   return (
-    <>
-      <Hero />
-      <ProjectsCarousel />
-      <Story />
-      <Line />
-      <Services />
-      <Line />
-      <Experience />
-      <Line />
-      {/* <Recommendations /> */}
-      <Contact />
-    </>
+    <div className="grid min-h-[calc(100vh-48px)] place-items-center md:min-h-[calc(100vh-65px)]">
+      <div className="mx-auto max-w-4xl px-6 pt-12 pb-24 sm:px-8 sm:pt-16 sm:pb-36">
+        <h1 className="text-primary xs:text-3xl font-serif text-3xl leading-tight sm:text-4xl md:text-5xl lg:text-6xl">
+          Hey, I&apos;m Sajad.
+          <br />
+          Developer & Designer
+        </h1>
+        <p className="text-foreground mt-4 text-lg text-balance md:text-xl md:leading-relaxed">
+          I&apos;m a passionate developer and designer interested in JavaScript,
+          TypeScript, Rust, Product Design, Startups, Web 3.0 and OSS.
+        </p>
+        <div className="xs:flex-row mt-8 flex flex-col items-center gap-2">
+          <Button className="xs:w-auto w-full" asChild>
+            <Link
+              href="https://cal.com/sajadevo/talk-with-sajad"
+              target="_blank"
+            >
+              Book a call
+            </Link>
+          </Button>
+          <Button
+            variant="secondary"
+            className="xs:w-auto w-full gap-3"
+            asChild
+          >
+            <Link href="https://contra.com/sajadevo" target="_blank">
+              <Contra className="size-4 shrink-0" /> Hire me on Contra
+            </Link>
+          </Button>
+        </div>
+        <div className="mt-12 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {services.map(({ title, description }) => (
+            <div
+              key={title}
+              className="border-secondary relative rounded-2xl border px-6 py-5"
+            >
+              <h2 className="mb-2 font-serif text-xl text-black">{title}</h2>
+              <p className="text-foreground text-base text-balance">
+                {description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
