57fa2578e90b4db88b1773da7b58d3fea94cae93
sajadevo <nawabisajadahmad@gmail.com>
2025-01-18 18:37:05 +0500

    update button

--- components/button.tsx
+++ components/button.tsx
@@ -1,28 +1,44 @@
 import React from "react";
 
+// @components
+import { Slot } from "@radix-ui/react-slot";
+
 // @utils
 import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "h-12 rounded-full px-6 border inline-flex items-center whitespace-nowrap overflow-hidden justify-center text-base text-center font-medium cursor-pointer disabled:opacity-70 disabled:pointer-events-none transition-all duration-200",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-white border-primary hover:opacity-85",
+        secondary:
+          "bg-transparent text-primary border-secondary hover:bg-secondary/25",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  ref?: React.Ref<HTMLButtonElement>;
+}
 
 export function Button({
   ref,
-  variant = "primary",
+  variant,
   className,
+  asChild = false,
   ...props
-}: React.ComponentProps<"button"> & {
-  ref?: React.ComponentRef<"button">;
-  variant?: "primary" | "secondary";
-  className?: string;
-}) {
-  const styles = cn(
-    "h-12 rounded-[10px] px-5 border inline-flex items-center whitespace-nowrap overflow-hidden justify-center text-base text-center font-medium cursor-pointer disabled:opacity-70 disabled:pointer-events-none transition-[color_opacity] duration-300",
-    {
-      "bg-primary text-white border-primary hover:opacity-85":
-        variant === "primary",
-      "bg-transparent text-primary border-secondary hover:bg-secondary/25":
-        variant === "secondary",
-    },
-    className
-  );
+}: ButtonProps) {
+  const Component = asChild ? Slot : "button";
+  const styles = cn(buttonVariants({ variant, className }));
 
-  return <button ref={ref} className={styles} {...props} />;
+  return <Component ref={ref} className={styles} {...props} />;
 }
