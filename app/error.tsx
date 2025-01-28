30fe3471b9173578a398c0611125435a2375845d
sajadevo <nawabisajadahmad@gmail.com>
2025-01-29 00:15:38 +0500

    add loading, error and 404 pages

---
+++ app/error.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+
+// @components
+import Link from "next/link";
+import { Button } from "@/components/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  React.useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="grid min-h-[calc(100vh-48px)] place-items-center md:min-h-[calc(100vh-65px)]">
+      <div className="mx-auto max-w-2xl px-6 pt-12 pb-24 text-center sm:px-8 sm:pt-16 sm:pb-36">
+        <h1 className="text-2xl leading-tight font-semibold text-balance text-black sm:text-3xl md:text-4xl">
+          Something went wrong!
+        </h1>
+        <p className="text-foreground mt-4 mb-8 text-base text-balance sm:text-lg md:leading-relaxed">
+          An error occurred while trying to load the page.
+        </p>
+        <div className="xs:flex-row flex flex-col items-center justify-center gap-3">
+          <Button className="xs:w-auto w-full" onClick={() => reset()}>
+            Try agian
+          </Button>
+          <Button variant="secondary" className="xs:w-auto w-full" asChild>
+            <Link
+              href="https://cal.com/sajadevo/talk-with-sajad"
+              target="_blank"
+            >
+              Return home
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+// @components
+import Link from "next/link";
+import { Button } from "@/components/button";
+
+export default function Loading() {
+  return (
+    <div className="grid min-h-[calc(100vh-48px)] place-items-center md:min-h-[calc(100vh-65px)]">
+      <div className="mx-auto max-w-2xl px-6 pt-12 pb-24 text-center sm:px-8 sm:pt-16 sm:pb-36">
+        <h1 className="text-2xl leading-tight font-semibold text-balance text-black sm:text-3xl md:text-4xl">
+          Page Not Found
+        </h1>
+        <p className="text-foreground mt-4 mb-8 text-base text-balance sm:text-lg md:leading-relaxed">
+          The page you are looking for does not exist.
+        </p>
+        <Button className="mx-auto" asChild>
+          <Link href="https://cal.com/sajadevo/talk-with-sajad" target="_blank">
+            Return home
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
