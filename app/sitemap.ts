67d28946d7f202a55344a44e61a19ac87ef02559
sajadevo <nawabisajadahmad@gmail.com>
2025-04-09 20:24:40 +0500

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ const domain = isProd
   ? process.env.NEXT_PUBLIC_PROD_URL!
   : process.env.NEXT_PUBLIC_DEV_URL!;
 
-const routes = ["", "about", "projects", "uses", "contact", "hello"];
+const routes = ["", "about", "projects", "uses", "contact"];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogPosts = await getBlogPosts();
