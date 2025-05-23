9c663713534f6d15f082692b2c5998a19d8aace8
Sajad <nawabisajadahmad@gmail.com>
2025-05-23 21:22:34 +0430

    remote resend

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,8 +1,6 @@
 "use server";
 
-import { EmailTemplate } from "@/components/email-template";
 import fs from "node:fs";
-import { Resend } from "resend";
 import matter from "gray-matter";
 
 export async function getBlogPosts(search?: string) {
@@ -35,33 +33,4 @@ export async function getBlogPosts(search?: string) {
   });
 
   return sortedPosts;
-}
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function sendInquiry({
-  name,
-  email,
-  message,
-}: {
-  name: string;
-  email: string;
-  message: string;
-}) {
-  try {
-    const { error } = await resend.emails.send({
-      from: process.env.RESEND_FROM_EMAIL!,
-      to: [process.env.RESEND_TO_EMAIL!],
-      subject: "New Inquiry from sajadevo.com",
-      react: EmailTemplate({ name, email, message }),
-    });
-
-    if (error) {
-      return { error: error.message, sent: false };
-    }
-
-    return { error: null, sent: true };
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
 }
