d14da9178aae1b7ddc719d82e94215d3341c1968
Sajad <nawabisajadahmad@gmail.com>
2025-06-19 00:29:35 +0400

    add blog and bits to the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ const domain = isProd
   ? process.env.NEXT_PUBLIC_PROD_URL!
   : process.env.NEXT_PUBLIC_DEV_URL!;
 
-const routes = ["", "about", "projects", "uses"];
+const routes = ["", "about", "projects", "blog", "bits", "uses"];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogPosts = await getBlogPosts();
